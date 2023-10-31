d64c6a7c0b752cfae6372f21ea9b82467d063d78
tanish bansal <tanish27bansal@gmail.com>
2023-10-31 13:00:33 +0530

    Update dpk.js

--- dpk.js
+++ dpk.js
@@ -25,13 +25,11 @@ exports.deterministicPartitionKey = (event) => {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
   return candidate;
-  
 };
 
 exports.refactoredDeterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-
   const createHash = (input)=>{
     if(typeof input !== "string" || !event?.partitionKey) { // for event objects excluding obejcts coming from partitionKey
       input = JSON.stringify(input);
@@ -50,5 +48,4 @@ exports.refactoredDeterministicPartitionKey = (event) => {
   candidate = candidate?candidate:event;// if partitionKey not found set input to event
   candidate = candidate?createHash(candidate):TRIVIAL_PARTITION_KEY; // if input is set, hashit otherwise set it to trivial
   return candidate;
-  
 };
